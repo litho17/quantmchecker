569a1f711e174fb61ae5f67b34dbbc33d4704d68
Tianhan Lu <tianhan.lu@colorado.edu>
2018-03-28 16:59:57 -0400

    add scala parser combinator in classpath of the script

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ export PATH=$HOME/Documents/workspace/checker-framework-2.4.0/checker/bin:$PATH
 
 #javac -cp .:/Users/lumber/.sbt/preloaded/org.scala-lang/scala-library/2.12.1/jars/scala-library.jar -AprintErrorStack -processor plv.colorado.edu.quantmchecker.QuantmChecker ~/Documents/workspace/quantmchecker/src/test/java/unit/MotivatingExample.java
 
-scala_lib="$HOME/.sbt/preloaded/org.scala-lang/scala-library/2.12.1/jars/scala-library.jar"
 class_files_location="target/scala-2.12/classes"
+
 fully_qualified_name_prefix="plv.colorado.edu."
 QUANTMCHECKER="quantmchecker.QuantmChecker"
 LISTADDCHECKER="listaddchecker.ListaddChecker"
@@ -15,6 +15,11 @@ LISTADDCHECKER="listaddchecker.ListaddChecker"
 lib_path=`python scripts/findlibs.py "$1"` # absolute path
 #echo $lib_path
 
+scala_lib="$HOME/.sbt/preloaded/org.scala-lang/scala-library/2.12.1/jars/scala-library.jar"
+scala_parser_lib="$HOME/.ivy2/cache/org.scala-lang.modules/scala-parser-combinators_2.12/bundles/scala-parser-combinators_2.12-1.1.0.jar"
+classpath=".:$lib_path:$scala_lib:$scala_parser_lib"
+
+
 pwd=`pwd`
 cd $class_files_location
 src_dir="$pwd/$2" # relative path
@@ -22,6 +27,6 @@ src_dir="$pwd/$2" # relative path
 mkdir output/
 
 #	set -x
-javac -cp ".:$lib_path:$scala_lib" -AprintErrorStack -processor $fully_qualified_name_prefix$QUANTMCHECKER `find $src_dir -name "*.java"` -d output/
+javac -cp $classpath -AprintErrorStack -processor $fully_qualified_name_prefix$QUANTMCHECKER `find $src_dir -name "*.java"` -d output/
 
 #-AignoreRawTypeArguments
