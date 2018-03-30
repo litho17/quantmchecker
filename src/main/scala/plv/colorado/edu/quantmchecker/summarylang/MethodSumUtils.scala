cd92f6595a70128e6828ac8f66ffb02a6d14efb7
Tianhan Lu <tianhan.lu@colorado.edu>
2018-03-30 17:21:19 -0400

    add a helper: which variable (local or field) is changed according to a method summary

--- src/main/scala/plv/colorado/edu/quantmchecker/summarylang/MethodSumUtils.scala
+++ src/main/scala/plv/colorado/edu/quantmchecker/summarylang/MethodSumUtils.scala
@@ -1,8 +1,32 @@
 package plv.colorado.edu.quantmchecker.summarylang
 
+import javax.lang.model.element.ExecutableElement
+
+import scala.collection.JavaConverters._
+
 /**
   * @author Tianhan Lu
   */
 object MethodSumUtils {
-
+  /**
+    *
+    * @param summary       a method summary
+    * @param invokedMethod a method invocation
+    * @return Either the index of formal argument that is described by the summary,
+    *         or the class field that is described by the summary
+    */
+  def whichVar(summary: MethodSummary, invokedMethod: ExecutableElement): Either[Integer, String] = {
+    println(invokedMethod.getSimpleName, invokedMethod.getParameters)
+    val formalArgs = invokedMethod.getParameters.asScala.map(v => v.getSimpleName.toString)
+    val varName = summary match {
+      case MethodSummaryI(v, _) => v
+      case MethodSummaryV(v, _) => v
+      case _ => ""
+    }
+    if (formalArgs.contains(varName)) {
+      Left(formalArgs.indexOf(varName))
+    } else {
+      Right(varName)
+    }
+  }
 }
