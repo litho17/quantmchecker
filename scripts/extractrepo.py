db1071bbc224b670a07cf7c62e5c8efab2d1ca71
Tianhan Lu <tianhan.lu@colorado.edu>
2018-05-01 08:18:25 -0600

    extractrepo.py: Only the first 1000 search results are available

--- scripts/extractrepo.py
+++ scripts/extractrepo.py
@@ -8,24 +8,35 @@ import ssl
 # data = json.load(open('a.json'))
 # print(ssl.OPENSSL_VERSION)
 
-auth = ('litho17', 'orc19940119')
-params = (
-    ('q', 'import org.springframework.boot'),
-    ('type', 'Code'),
-)
-url = "https://api.github.com/search/code"
-data = requests.get(url, auth=auth, params=params).json()
+for pageNum in range(1, 1000000):
+	auth = ('litho17', 'orc19940119')
+	params = (
+	    ('q', 'import org.springframework.boot'),
+	    ('type', 'Code'),
+	    ('language', 'java'),
+	    ('per_page', '100'),
+	    ('page', pageNum)
+	)
+	url = "https://api.github.com/search/code"
+	print(pageNum)
+	data = requests.get(url, auth=auth, params=params).json()
 
-print((data["items"]))
-for item in data["items"]:
-	# for i in item["repository"]:
-	#	print i
-	url = item["repository"]["url"]
-	response = requests.get(url, auth=auth)
-	json = response.json()
-	# print(json["stargazers_count"])
-	if json["stargazers_count"] > 10:
-		print(json["html_url"])
+	try:
+		# print(len(data["items"]))
+		for item in data["items"]:
+			# for i in item["repository"]:
+			#	print i
+			url = item["repository"]["url"]
+			response = requests.get(url, auth=auth)
+			json = response.json()
+			# print(json["stargazers_count"])
+			if json["stargazers_count"] > 10:
+				print(json["html_url"])
+	except Exception as inst:
+		print(data)
+		print(type(inst))
+		print(inst.args)
+		print(inst)
 
 # References: 
 # https://curl.trillworks.com/
