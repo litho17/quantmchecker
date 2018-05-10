bc0dc56df3dd1a52265e7ef1bf9a1522f754c8c9
Tianhan Lu <tianhan.lu@colorado.edu>
2018-05-10 13:41:02 -0600

    add a script that sets up the environment variables

---
+++ scripts/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+checkerdir="$HOME/Documents/workspace/quantmchecker"
+LIB="$checkerdir/lib"
+class_files_location="$checkerdir/target/scala-2.12/classes"
+
+export PATH=$checker_framework_bin:$PATH
+export LD_LIBRARY_PATH=$LIB:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$LIB:$DYLD_LIBRARY_PATH
+
+pwd=`pwd`
+cd $class_files_location
+jar -cvf qc.jar plv &> /dev/null
+mv qc.jar $HOME/Desktop/
+cd $pwd
