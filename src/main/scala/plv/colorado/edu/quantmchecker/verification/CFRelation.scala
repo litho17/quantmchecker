052ba4bf4936ccf0ad931e4929b9065a1328f834
Tianhan Lu <tianhan.lu@colorado.edu>
2018-09-28 10:26:36 -0600

    CFRelation: Loop body and loop head has no relations; Get relations also from Case/Catch tree; Count update ssttmts in for loop

--- src/main/scala/plv/colorado/edu/quantmchecker/verification/CFRelation.scala
+++ src/main/scala/plv/colorado/edu/quantmchecker/verification/CFRelation.scala
@@ -5,6 +5,7 @@ import com.sun.source.tree._
 import plv.colorado.edu.Utils
 
 import scala.collection.JavaConverters._
+import scala.collection.immutable.HashSet
 
 /**
   * @author Tianhan Lu
@@ -12,64 +13,71 @@ import scala.collection.JavaConverters._
 class CFRelation(tree: StatementTree, solver: Z3Solver) {
   val DEFAULT_LOOP_BOUND = 1000
 
-  val constraints: List[AST] = treeToCons(tree)
+  val constraints: Set[AST] = treeToCons(tree)
 
-  private def genEqCons(stmts: Set[Tree]): List[AST] = {
+  private def genEqCons(stmts: List[_ <: Tree]): Set[AST] = {
     if (stmts.nonEmpty) {
       val headHashcode = Utils.hashCode(stmts.head)
       val head = solver.getNatVar(headHashcode)
-      stmts.tail.foldLeft(List[AST]()) {
+      stmts.tail.foldLeft(new HashSet[AST]) {
         (acc, stmt) => // head = s1, head = s2, ...
           val eq = solver.mkEq(head, solver.getNatVar(Utils.hashCode(stmt)))
-          eq :: acc
+          acc + eq
       }
-    } else List.empty
+    } else new HashSet[AST]
   }
 
-  def treeToCons(tree: CaseTree): List[AST] = genEqCons(tree.getStatements.asScala.toSet + tree)
+  private def treeToCons(tree: CaseTree): Set[AST] = {
+    val eq = genEqCons(tree.getStatements.asScala.toList :+ tree)
+    tree.getStatements.asScala.foldLeft(eq) { (acc, s) => acc ++ treeToCons(s) }
+  }
 
-  def treeToCons(tree: CatchTree): List[AST] = genEqCons(tree.getBlock.getStatements.asScala.toSet + tree)
+  private def treeToCons(tree: CatchTree): Set[AST] = {
+    val eq = genEqCons(tree.getBlock.getStatements.asScala.toList :+ tree.getParameter)
+    treeToCons(tree.getBlock) ++ eq
+  }
 
   /**
     *
     * @param tree a statement tree (AST node)
     * @return a set of String constraints that describe the control flow relations inside the target AST node
     */
-  def treeToCons(tree: StatementTree): List[AST] = {
-    if (tree == null)
-      return List[AST]()
+  private def treeToCons(tree: StatementTree): Set[AST] = {
+    if (tree == null) return new HashSet[AST]
 
-    def genLoopCons(loop: StatementTree, body: StatementTree): List[AST] = {
-      if (loop.isInstanceOf[DoWhileLoopTree] || loop.isInstanceOf[EnhancedForLoopTree] || loop.isInstanceOf[WhileLoopTree]) { // block >= this
-        val bodyVar = solver.getNatVar(Utils.hashCode(body))
-        val loopVar = solver.getNatVar(Utils.hashCode(loop))
-        val ge = solver.mkGe(bodyVar, loopVar)
-        ge :: treeToCons(body)
-      } else
-        List[AST]()
+    def genLoopCons(loop: StatementTree, body: StatementTree): Set[AST] = {
+      if (loop.isInstanceOf[DoWhileLoopTree] || loop.isInstanceOf[EnhancedForLoopTree] || loop.isInstanceOf[WhileLoopTree]) {
+        // val bodyVar = solver.getNatVar(Utils.hashCode(body))
+        // val loopVar = solver.getNatVar(Utils.hashCode(loop))
+        // val ge = solver.mkGe(bodyVar, loopVar)
+        // ge :: treeToCons(body)
+        treeToCons(body)
+      } else new HashSet[AST]
     }
 
     val thisHashcode = Utils.hashCode(tree)
     val me = solver.getNatVar(thisHashcode)
     tree match {
       case stmt: BlockTree =>
-        stmt.getStatements.asScala.foldLeft(genEqCons(stmt.getStatements.asScala.toSet + stmt)) {
-          (acc, s) => acc ::: treeToCons(s)
+        stmt.getStatements.asScala.foldLeft(genEqCons(stmt.getStatements.asScala.toList :+ stmt)) {
+          (acc, s) => acc ++ treeToCons(s)
         }
       case stmt: DoWhileLoopTree => genLoopCons(stmt, stmt.getStatement)
       case stmt: EnhancedForLoopTree => genLoopCons(stmt, stmt.getStatement)
       case stmt: WhileLoopTree => genLoopCons(stmt, stmt.getStatement)
-      case stmt: ForLoopTree => // this = init1 = init2 = ...; block >= this
-        val initCons = genEqCons(stmt.getInitializer.asScala.toSet + tree)
+      case stmt: ForLoopTree => // this = init1 = init2 = ...
+        val initCons = genEqCons(stmt.getInitializer.asScala.toList :+ tree)
         val body = solver.getNatVar(Utils.hashCode(stmt.getStatement))
-        val ge = solver.mkGe(body, me)
-        ge :: initCons ::: treeToCons(stmt.getStatement)
+        val update = solver.getNatVar(Utils.hashCode(stmt.getUpdate))
+        val eq = solver.mkEq(body, update) // body = update
+        // val ge = solver.mkGe(body, me)
+        initCons ++ treeToCons(stmt.getStatement) + eq
       case stmt: LabeledStatementTree => // this = body; this = label
         val body = solver.getNatVar(Utils.hashCode(stmt.getStatement))
         val label = solver.getNatVar(stmt.getLabel.toString)
         val eq1 = solver.mkEq(me, body)
         val eq2 = solver.mkEq(me, label)
-        eq1 :: eq2 :: treeToCons(stmt.getStatement)
+        treeToCons(stmt.getStatement) + eq1 + eq2
       case stmt: IfTree => // this = then + else
         val thenVar = solver.getNatVar(Utils.hashCode(stmt.getThenStatement))
         val eq =
@@ -77,7 +85,7 @@ class CFRelation(tree: StatementTree, solver: Z3Solver) {
             val elseVar = solver.getNatVar(Utils.hashCode(stmt.getElseStatement))
             solver.mkEq(me, solver.mkAdd(thenVar, elseVar))
           } else solver.mkEq(me, thenVar)
-        eq :: treeToCons(stmt.getThenStatement) ::: treeToCons(stmt.getElseStatement)
+        treeToCons(stmt.getThenStatement) ++ treeToCons(stmt.getElseStatement) + eq
       case stmt: SwitchTree => // this = case1 + case2 + ...
         val cases = {
           val cases = stmt.getCases.asScala
@@ -86,12 +94,10 @@ class CFRelation(tree: StatementTree, solver: Z3Solver) {
           else solver.mkAdd(cases.map(casetree => solver.getNatVar(Utils.hashCode(casetree))).toArray: _*)
         }
         val eq = solver.mkEq(me, cases)
-        stmt.getCases.asScala.foldLeft(List[AST](eq)) {
-          (acc, casetree) => acc ::: treeToCons(casetree)
-        }
-      case stmt: ExpressionStatementTree => List[AST]()
-      case stmt: ReturnTree => List[AST]()
-      case stmt: VariableTree => List[AST]()
+        stmt.getCases.asScala.foldLeft(HashSet[AST](eq)) { (acc, casetree) => acc ++ treeToCons(casetree) }
+      case stmt: ExpressionStatementTree => new HashSet[AST]
+      case stmt: ReturnTree => new HashSet[AST]
+      case stmt: VariableTree => new HashSet[AST]
       case stmt: TryTree => // this = try; this = finally; this = catch1 + catch2 + ...
         val tryVar = solver.getNatVar(Utils.hashCode(stmt.getBlock))
         val eq1 = solver.mkEq(me, tryVar)
@@ -106,11 +112,11 @@ class CFRelation(tree: StatementTree, solver: Z3Solver) {
           else if (catches.size == 1) solver.mkEq(me, solver.getNatVar(Utils.hashCode(catches.head)))
           else solver.mkEq(me, solver.mkAdd(catches.map(catchtree => solver.getNatVar(Utils.hashCode(catchtree))).toArray: _*))
         }
-        stmt.getCatches.asScala.foldLeft(List[AST](eq1, eq2, eq3)) {
-          (acc, catchtree) => acc ::: treeToCons(catchtree)
+        stmt.getCatches.asScala.foldLeft(HashSet[AST](eq1, eq2, eq3)) {
+          (acc, catchtree) => acc ++ treeToCons(catchtree)
         } ++ treeToCons(stmt.getBlock) ++ treeToCons(stmt.getFinallyBlock)
       case stmt: SynchronizedTree => treeToCons(stmt.getBlock)
-      case _ => List[AST]()
+      case _ => new HashSet[AST]
     }
   }
 }
