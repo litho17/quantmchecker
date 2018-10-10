fe475f2ad509aadd6873f114fcd485e64d27513d
Tianhan Lu <tianhan.lu@colorado.edu>
2018-10-10 16:39:57 -0600

    add image_processor

---
+++ src/test/java/image_processor/com/stac/image/ImageAlgorithmStore.java
@@ -0,0 +1,37 @@
+package image_processor.com.stac.image;
+
+import image_processor.com.stac.image.algorithms.detectors.*;
+import image_processor.com.stac.image.algorithms.filters.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+public class ImageAlgorithmStore {
+    private static Map<String, ImageAlgorithm> store = new HashMap<>();
+
+    static {
+        addAlgorithm(BlackDetector.class);
+        addAlgorithm(BlueDetector.class);
+        addAlgorithm(EdgingDetector.class);
+        addAlgorithm(GreenDetector.class);
+        addAlgorithm(RedDetector.class);
+        addAlgorithm(WhiteDetector.class);
+        addAlgorithm(Intensify.class);
+        addAlgorithm(Invert.class);
+    }
+
+    private static void addAlgorithm(Class<? extends ImageAlgorithm> algo) {
+        try {
+            store.put(algo.getSimpleName(), algo.newInstance());
+        } catch (InstantiationException|IllegalAccessException e) {
+            throw new RuntimeException("Fatal initialization error has occurred");
+        }
+    }
+
+    public static ImageAlgorithm getAlgorithm(String name) {
+        return store.get(name);
+    }
+}
---
+++ src/test/java/image_processor/com/stac/learning/VectorFactory.java
@@ -0,0 +1,8 @@
+package image_processor.com.stac.learning;
+
+/**
+ *
+ */
+public abstract class VectorFactory {
+    abstract public Vector get(int n);
+}
